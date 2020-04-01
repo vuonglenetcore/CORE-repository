a6cc11e1c8257b077bfd9f8e9f66d43de8b45098
Admin <Admin@DESKTOP-K9E5QJ9>
2020-04-01 10:25:35 +0700

    configuration

---
+++ coreShop/coreShop.Data/Enums/Status.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace coreShop.Data.Enums
+{
+    public enum Status
+    {
+        InActive,
+        Active
+    }
+}
