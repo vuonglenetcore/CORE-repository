0359180effd3d67d4d2d864014a1e5866ead6489
Admin <Admin@DESKTOP-K9E5QJ9>
2020-04-01 11:03:37 +0700

    migration db create database

---
+++ coreShop/README.md
@@ -0,0 +1,14 @@
+﻿# ASP.NET Core 3.1 project from TEDU
+## Technologies
+- ASP.NET Core 3.1
+- Entity Framework Core 3.1
+## Install Packages --data
+- Microsoft.EntityFrameworkCore.SqlServer
+- Microsoft.EntityFrameworkCore.Tools
+- Microsoft.EntityFrameworkCore.Design
+
+- Microsoft.Extensions.Configuration.FileExtensions sử dụng cho SetBasePath
+- Microsoft.Extensions.Configuration.Json sử dụng cho AddJsonFile
+## Youtube tutorial
+## How to configure and run
+## How to contribute
---
+++ coreShop/coreShop.Data/EF/coreShopDbContextFactory.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace coreShop.Data.EF
+{
+    public class coreShopDbContextFactory : IDesignTimeDbContextFactory<coreShopDbContext>
+    {
+        public coreShopDbContext CreateDbContext(string[] args)
+        {
+            IConfigurationRoot configuration = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json")
+                .Build();
+
+            var connectionString = configuration.GetConnectionString("coreShopSolutionDb");
+
+            var optionsBuilder = new DbContextOptionsBuilder<coreShopDbContext>();
+            optionsBuilder.UseSqlServer(connectionString);
+
+            return new coreShopDbContext(optionsBuilder.Options);
+        }
+    }
+}
